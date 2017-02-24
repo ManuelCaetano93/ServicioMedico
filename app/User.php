5ebaf8d0838b1899fc31aae9613f2dc445eb6334
ManuelCaetano93FG <manuelcaetano93@gmail.com>
2017-02-24 18:22:12 -0400

    For Now

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'surname', 'identification', 'birthday', 'sex', 'phone', 'cellphone', 'residence', 'email', 'password'
+        'name', 'surname', 'identification', 'birthday', 'sex', 'phone', 'cellphone', 'residence', 'email'
     ];
 
     public function userAppointments(){
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -24,7 +24,38 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('123123'),
             'updated_at' => \Carbon\Carbon::now(),
             'created_at' => \Carbon\Carbon::now(),
+        ]);
 
+        // Not On Final Release
+
+        DB::table('users')->insert([
+            'name' => 'Pedro',
+            'surname' => 'Ramirez',
+            'identification' => 'V11122233',
+            'birthday' => '12313',
+            'sex' => 'male',
+            'phoneNumber' => '123123',
+            'cellphoneNumber' => '123123',
+            'residence' => 'Caracas',
+            'email' => 'PedroRamirez@gmail.com',
+            'password' => bcrypt('123123'),
+            'updated_at' => \Carbon\Carbon::now(),
+            'created_at' => \Carbon\Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Betty',
+            'surname' => 'Ramirez',
+            'identification' => 'V3322211',
+            'birthday' => '12313',
+            'sex' => 'female',
+            'phoneNumber' => '123123',
+            'cellphoneNumber' => '123123',
+            'residence' => 'Caracas',
+            'email' => 'BettyRamirez@gmail.com',
+            'password' => bcrypt('123123'),
+            'updated_at' => \Carbon\Carbon::now(),
+            'created_at' => \Carbon\Carbon::now(),
         ]);
     }
 }
