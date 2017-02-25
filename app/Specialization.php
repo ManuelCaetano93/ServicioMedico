32525978d579f7c02c86493654ed029961749ebe
ManuelCaetano93 <manuelcaetano93@gmail.com>
2017-02-25 00:03:51 -0400

    Changes to seeder, first specialization model made

--- app/Specialization.php
+++ app/Specialization.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Specialization extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+    ];
+
+    public function userAppointemnts(){
+        return $this->belongsTo('App/User', 'id_user_specialization');
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
+		$this->call(UsersTableSeeder::class);
+		$this->call(RoleHasPermissionsTableSeeder::class);
     }
 }
