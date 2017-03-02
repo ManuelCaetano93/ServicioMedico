aa477a657ab52aebada945d478e066f476547550
SabinaRosc <sabinarosciano@gmail.com>
2017-03-02 16:53:40 -0400

    ahora el modal muestra el nombre

--- resources/views/medicines/index.blade.php
+++ resources/views/medicines/index.blade.php
@@ -75,7 +75,7 @@
                     <div class="modal-body">
                         <p>¿Seguro que desea eliminar este
                             registro?</p>
-                        <p class="name">{{ url('/medicines/'.$medicine->id) }}</p>
+                        <p class="name">{{ $medicine->name }}</p>
                     </div>
                     <div class="modal-footer">
                         <form class="form-inline form-delete"
