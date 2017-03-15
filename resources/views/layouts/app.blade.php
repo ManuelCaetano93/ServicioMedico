726c5b3c816e3f2c9e8e10a3b7a116923265b644
SabinaRosc <sabinarosciano@gmail.com>
2017-03-15 12:16:47 -0400

    hardcore derp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,12 +10,9 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{asset('css/font-awesome.css')}}" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css"
-          integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ asset('css/style.css')}}" rel="stylesheet"/>
-
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
@@ -28,12 +25,17 @@
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{ url('/') }}"><img src="img/logo.png" alt="logo"/></a>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
             </div>
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
@@ -55,9 +57,8 @@
                         <li><a href="{{ url('/records') }}">Historia Medica</a></li>
                         <li><a href="{{ url('/medicines') }}">Medicinas</a></li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false">
-                                {{ Auth::user()->name }}
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu" role="menu">
                                 <li>
@@ -66,8 +67,7 @@
                                                     document.getElementById('logout-form').submit();">
                                         Logout
                                     </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                          style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         {{ csrf_field() }}
                                     </form>
                                 </li>
@@ -80,8 +80,6 @@
     </nav>
     @yield('content')
 </div>
-@extends('footer')
-
 <!-- Scripts -->
 <script src="/js/app.js"></script>
 <script src="{{ asset("js/jquery/jquery.min.js") }}"></script>
--- resources/views/specializations/index.blade.php
+++ resources/views/specializations/index.blade.php
@@ -17,12 +17,14 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <div class="row">
+                        <div class="row fa-align-center">
                             <div class="col-xs-4"><h5>Especializaciones</h5></div>
-                            <div class="col-xs-8 text-right">
+                            <div class="col-xs-6 text-right">
                                 <a href="{{ url('/specializations/create') }}" class="btn btn-success">Nueva
                                     Especializacion
                                 </a>
+                            </div>
+                            <div class="col-xs-2 text-right">
                                 <a href="{{ url('/specializations/deleted') }}" class="btn btn-success">Recuperar
                                 </a>
                             </div>
@@ -33,35 +35,43 @@
                             <div class="col-sm-4">
                                 <div class="card">
                                     <div class="card-block">
-                                        <h4 class="card-title">{{ $specialization->name }}</h4>
+                                        <div>
+                                            <h4 class="card-title">{{ $specialization->name }}</h4>
+                                        </div>
                                         <hr>
-                                        <div class="text-center">
-                                            <a href="{{ url('specializations/'.$specialization->id.'/edit') }}"
-                                               class="btn btn-primary">
-                                                <i class="fa fa-edit"></i>
-                                            </a>
-                                            <button class="btn btn-danger"
-                                                    data-action="{{ url('/specializations/'.$specialization->id) }}"
-                                                    data-name="{{ $specialization->name }}"
-                                                    data-toggle="modal"
-                                                    data-target="#confirm-delete{{$specialization->id}}">
-                                                <i class="fa fa-trash fa-1x"></i>
-                                            </button>
+                                        <div class="row">
+                                            <div class="col-sm-6 text-center">
+                                                <a href="{{ url('specializations/'.$specialization->id.'/edit') }}"
+                                                   class="btn btn-primary">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                            </div>
+
+                                            <div class="col-sm-6 text-center">
+                                                <button class="btn btn-danger"
+                                                        data-action="{{ url('/specializations/'.$specialization->id) }}"
+                                                        data-name="{{ $specialization->name }}"
+                                                        data-toggle="modal"
+                                                        data-target="#confirm-delete{{$specialization->id}}">
+                                                    <i class="fa fa-trash fa-1x"></i>
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         @endforeach
+                        <tr>
+                            <td colspan="7" class="text-center">
+                                {{ $specializations->links() }}
+                            </td>
+                        </tr>
                     </div>
-                    <tr>
-                        <td colspan="7" class="text-center">
-                            {{ $specializations->links() }}
-                        </td>
-                    </tr>
                 </div>
             </div>
         </div>
     </div>
+<<<<<<< HEAD
     </div>
     @foreach($specializations as $specialization)
         <div class="modal fade" id="confirm-delete{{$specialization->id}}" tabindex="-1"
@@ -97,4 +107,6 @@
             </div>
         </div>
     @endforeach
+=======
+>>>>>>> parent of c6861bc... Merge pull request #19 from ManuelCaetano93FG/Dev_sabina
 @endsection
